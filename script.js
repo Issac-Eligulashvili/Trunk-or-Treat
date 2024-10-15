c06b08fb0d8f6d5681c7989a20acf59130a39931
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-10-15 13:48:47 -0400

    stuff is done

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 $(document).ready(function () {
      $('.custom-nav').hide();
      $('#subtext').hide();
+     $('.showcaseDescription').hide();
+
      $('#burgerMenu').on('click', () => {
           $('.custom-nav').slideDown()
+          $('body').addClass('overflow-hidden');
      })
 
-     $('#closeBtn').on('click', () => {
+     $('#closeBtn, .navLink').on('click', () => {
           $('.custom-nav').slideUp(); //close the navbar!
+          $('body').removeClass('overflow-hidden');
      })
 
      $('.moreInfoBtn').on('click', () => {
           $('#subtext').toggle(50);
      })
+
+     $('.showcaseImage').on('mouseenter', (event) => {
+          $(event.target).next().fadeIn();
+     }).on('mouseleave', (event) => {
+          $(event.target).next().fadeOut();
+     });
 });
